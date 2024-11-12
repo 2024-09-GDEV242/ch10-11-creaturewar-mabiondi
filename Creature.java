f7fd99b2714e66ca12d03025034dc76c5fd2701d
mabiondi <mbiondi189@gmail.com>
2024-11-12 17:53:48 -0500

    Updated Creature docs

--- Creature.java
+++ Creature.java
@@ -7,8 +7,8 @@
  * the creature is alive or knocked out. The creature is also responsible for calculating
  * damage delivered based on the creature's strength (1 to str) 
  * 
- * @author Crosbie
- * @version 2020-10 v1.0
+ * @author Crosbie, updated by Michael Biondi
+ * @version 2024-11 v1.1
  */
 // we will learn what the abstract keyword does in a later chapter
 public abstract class Creature
@@ -37,7 +37,8 @@ public abstract class Creature
      * @param hp the health of the creature at the start of the simulation, and the current health levels during battle
      */
     public Creature (int str, int hp) {
-       //implement this
+       this.str = str;
+       this.hp = hp;
     }
     
     

4b1871a76a97fa043249fcf12b03c6148276442e
mabiondi <mbiondi189@gmail.com>
2024-11-18 00:43:15 -0500

    Added Human unit test class

---
+++ HumanUnitTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test Class for the Human class.
+ *
+ * @author Michael Biondi
+ * @version 2024-11
+ */
+public class HumanUnitTest
+{
+    /**
+     * Default constructor for test class HumanUnitTest
+     */
+    public HumanUnitTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
